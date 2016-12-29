def356a6714138ceba0028e938ac5e8118b4b648
Hil Liao <hilliao@gmail.com>
2016-12-28 22:52:50 -0800

    react js tutorial from https://github.com/learncodeacademy/react-js-tutorials

---
+++ react-js-tutorials/1-basic-react/src/js/components/Layout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from 'axios';
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "title -> Welcome_" + (3 + 5),
+            msg: ''
+        };
+        this.name = "HIL";
+        this.springboot = '';
+    }
+
+    getVal() {
+        return 3 + '(int)';
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080')
+            .then(res => {
+                console.log(res.data);
+                this.setState(
+                    {
+                        msg: res.data
+                    }
+                )
+            });
+    }
+
+
+    changeTitle(title) {
+        this.setState({title});
+    }
+
+    render() {
+        //const name = "Hil";
+        return (
+            <div>
+                <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+                <h1> {this.name} {this.getVal()}  </h1>
+                <h2> -> {this.state.msg} {console.log(this.state.msg)} </h2>
+                <Footer />
+            </div>
+        );
+    }
+}
---
+++ react-js-tutorials/2-react-router/src/js/client.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Layout from "./pages/Layout";
+import Settings from "./pages/Settings";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
