1e39f25fc3dc0aacc3925cc3a2d4cb8e80f4d727
Hil Liao <hilliao@gmail.com>
2018-10-07 16:14:22 -0700

    deleted sample apps, upgraded starbucks online order app to Android studio 3.2, move C# apps to csharp/

--- nodejs/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    res.send('welcome to node js API');
-});
-var server = app.listen(port, function () {
-    console.log('running on port: ' + port);
-});
