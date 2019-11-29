d43b26d96dca462b5901b3b411c9e2d515671971
Hil Liao <hilliao@gmail.com>
2019-11-29 14:16:41 -0800

    modified infrastructure provisioning scripts to create cloud functions

--- gcp-compute/infra-setup.sh
+++ gcp-compute/infra-setup.sh
@@ -1,4 +1,7 @@
 # create cloud scheduler, pub/sub, cloud function
-
 gcloud pubsub topics create delete-expired-gce
-gcloud functions deploy delete_expired_gce --entry-point delete_expired --runtime python37 --trigger-topic delete-expired-gce
+
+PROJECT=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT" --format="value(PROJECT_NUMBER)")
+gcloud functions deploy delete-expired-gce --entry-point=main --runtime=python37 --trigger-topic=delete-expired-gce --service-account=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+
