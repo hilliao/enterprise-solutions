4ca1494bf8feddc014b75d830206de87a541b079
Hil Liao <hilliao@gmail.com>
2020-10-30 17:24:04 -0700

    Google cloud next 2019 demo app to import Google FIT user data into BigQuery for a better dashboard experience

---
+++ googlecloud/fit/old.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+import json
+from collections import OrderedDict
+
+import bottle_mysql
+import httplib2
+from bottle import *
+from googleapiclient.discovery import build
+from oauth2client import client
+
+import backend
+from update_google_fit import get_and_store_fit_data
+
+bad_activities = "(0,3,5,109,110,111,112,117,118)"
+
+# bottle web framework init
+app = Bottle()
+application = app
+# dbhost is optional, default is localhost
+plugin = bottle_mysql.Plugin(
+    dbhost=backend.config.get('database_config', 'dbhost'),
+    dbport=int(backend.config.get('database_config', 'dbport')),
+    dbuser=backend.config.get('database_config', 'dbuser'),
+    dbpass=backend.config.get('database_config', 'dbpass'),
+    dbname=backend.config.get('database_config', 'dbname'))
+app.install(plugin)
+
+
+def require_key():
+    key = request.query.get('key', '')
+    if key != backend.API_key:
+        backend.get_logger().log_text('request made with invalid API key', severity='WARNING')
+        abort(httplib.UNAUTHORIZED, "invalid API key")
+
+
+@app.get('/health')
+def health_check(db):
+    if db:
+        return "database connection alive: " + str(db.connection)
+    else:
+        backend.get_logger().log_text("database connection is in a bad state", severity='CRITICAL')
+        return HTTPError(httplib.INTERNAL_SERVER_ERROR, "database connection is in a bad state")
+
+
+@app.get('/')
+def default_get(db):
+    name = request.query.get('state', '')
+    p = request.urlparts
+    redirect_uri = "{}://{}{}".format(p.scheme, p.netloc, p.path)
+
+    flow = client.flow_from_clientsecrets(
+        backend.get_client_secret_filename(),
+        scope=["profile", "email",
+               'https://www.googleapis.com/auth/fitness.activity.read',
+               'https://www.googleapis.com/auth/fitness.body.read'],
+        redirect_uri=redirect_uri)
+    flow.params['access_type'] = 'offline'
+    flow.params['prompt'] = 'consent'
+    if 'code' not in request.query:
+        require_key()
+        if not name:
+            return static_file("index.html", ".")
+        auth_uri = flow.step1_get_authorize_url(state=name)
+        redirect(auth_uri)
+    else:
+        creds = flow.step2_exchange(code=request.query.code)
+        http_auth = creds.authorize(httplib2.Http())
+        user_info_service = build('oauth2', 'v2', http=http_auth)
+        u = user_info_service.userinfo().get().execute()
+        db.execute(
+            """
+            REPLACE INTO google_fit 
+            SET username=%s, google_id=%s, full_name=%s, gender=%s,
+             image_url=%s, email=%s, refresh_token=%s
+            """
+            , (name, u['id'], u['name'], u.get('gender'), u['picture']
+               , u['email'], creds.refresh_token)
+        )
+        steps, activity = get_and_store_fit_data(http_auth, db, name)
+        response.content_type = 'application/json'
+        return json.dumps(dict(steps), sort_keys=True, indent=4)
+
+
+@app.get('/steps_for_user/<name>')
+def steps_for_user(name, db):
+    require_key()
+    backend.get_logger().log_text("steps_for_user requested for: " + name, severity='INFO')
+    db.execute("SELECT day, steps FROM steps WHERE username=%s", (name,))
+    result = dict([(r['day'], r['steps']) for r in db.fetchall()])
+    response.content_type = 'application/json'
+    return json.dumps(result, sort_keys=True, indent=4)
+
+
+@app.get('/activity_for_user/<name>')
+def activity_for_user(name, db):
+    require_key()
+    backend.get_logger().log_text("activity_for_user requested for: " + name, severity='INFO')
+    db.execute(
+        """
+        SELECT a.day, ROUND(SUM(a.length_ms) / 1000 / 60) AS minutes 
+        FROM activity a 
+        INNER JOIN activity_types t ON a.activity_type=t.id 
+        WHERE a.username=%s AND a.activity_type NOT IN {} 
+        GROUP BY a.day
+        """.format(bad_activities), (name,))
+    result = dict([(r['day'], int(r['minutes'])) for r in db.fetchall()])
+    response.content_type = 'application/json'
+    return json.dumps(result, sort_keys=True, indent=4)
+
+
+@app.get('/users/<name>/activities')
+def user_activities(name, db):
+    require_key()
+    backend.get_logger().log_text("users/<name>/activities requested for: " + name, severity='INFO')
+    activities = query_activities(db, name)
+    response.content_type = 'application/json'
+    return json.dumps(activities, sort_keys=True, indent=4)
+
+
+def query_activities(db, name):
+    db.execute(
+        """
+        SELECT a.day, ROUND(a.length_ms / 1000 / 60) AS minutes,
+             t.name as activity_type 
+        FROM activity a 
+        INNER JOIN activity_types t ON a.activity_type=t.id 
+        WHERE a.username=%s
+        """,
+        (name,)
+    )
+    result = db.fetchall()
+    activities = {}
+    for r in result:
+        if r['day'] in activities:
+            if 'daily_activities' not in activities[r['day']]:
+                activities[r['day']]['daily_activities'] = []
+
+            activities[r['day']]['daily_activities'].append(
+                {"minutes": int(r['minutes']),
+                 "activity_type": r['activity_type']})
+
+        else:
+            activities[r['day']] = {}
+            activities[r['day']]['daily_activities'] = []
+            activities[r['day']]['daily_activities'].append(
+                {"minutes": int(r['minutes']),
+                 "activity_type": r['activity_type']})
+    return activities
+
+
+@app.get('/steps_for_user/last_week/<name>')
+def steps_for_user_last_week(name, db):
+    require_key()
+    backend.get_logger().log_text("steps_for_user/last_week requested for: " + name, severity='INFO')
+    db.execute(
+        """
+        SELECT SUM(steps) as sum 
+        FROM steps 
+        WHERE username=%s AND day >= date_sub(CURDATE(), INTERVAL 1 WEEK)
+        """
+        , (name,)
+    )
+    result = str(db.fetchone()['sum'])
+    return result
+
+
+@app.get('/steps_for_user/last_day/<name>')
+def steps_for_user_last_day(name, db):
+    require_key()
+    backend.get_logger().log_text("steps_for_user/last_day requested for: " + name, severity='INFO')
+    db.execute(
+        """
+        SELECT SUM(steps) as sum 
+        FROM steps 
+        WHERE username=%s AND day >= date_sub(CURDATE(), INTERVAL 1 DAY)
+        """, (name,)
+    )
+    result = str(db.fetchone()['sum'])
+    return result
+
+
+@app.get('/users')
+def get_users(db):
+    require_key()
+    backend.get_logger().log_text("user requested", severity='INFO')
+    db.execute("SELECT username FROM google_fit")
+    result = [u['username'] for u in db.fetchall()]
+    response.content_type = 'application/json'
+    return json.dumps(result, sort_keys=True, indent=4)
+
+
+@app.get('/step_leaderboard')
+def steps_leaderboard(db):
+    require_key()
+    backend.get_logger().log_text("step_leaderboard requested", severity='INFO')
+    db.execute(
+        """
+        SELECT username, SUM(steps) as steps 
+        FROM steps 
+        WHERE day > date_sub(CURDATE(), INTERVAL 1 WEEK) 
+        GROUP BY username 
+        ORDER BY steps DESC 
+        LIMIT 20
+        """
+    )
+    result = OrderedDict(
+        [(r['username'], int(r['steps'])) for r in db.fetchall()])
+    response.content_type = 'application/json'
+    return json.dumps(result, indent=4)
+
+
+@app.get('/activity_leaderboard')
+def activity_leaderboard(db):
+    require_key()
+    backend.get_logger().log_text("activity_leaderboard requested", severity='INFO')
+    db.execute(
+        """
+        SELECT username, ROUND(SUM(a.length_ms) / 1000 / 60) AS minutes 
+        FROM activity a 
+        INNER JOIN activity_types t ON a.activity_type=t.id 
+        WHERE day > date_sub(CURDATE(), INTERVAL 1 WEEK) AND
+            a.activity_type NOT IN {} 
+        GROUP BY username 
+        ORDER BY minutes DESC 
+        LIMIT 20
+        """.format(bad_activities)
+    )
+    result = OrderedDict([(r['username'],
+                           int(r['minutes'])) for r in db.fetchall()])
+    response.content_type = 'application/json'
+    return json.dumps(result, indent=4)
+
+
+@app.get('/combined_leaderboard')
+def combined_leaderboard(db):
+    require_key()
+    backend.get_logger().log_text("combined_leaderboard requested", severity='INFO')
+    db.execute(
+        """
+        SELECT s.username, s.steps, m.minutes
+        FROM (
+            SELECT username, SUM( steps ) AS steps
+            FROM steps
+            WHERE DAY > DATE_SUB( CURDATE( ) , INTERVAL 1 WEEK )
+            GROUP BY username
+            ) AS s
+        INNER JOIN (
+            SELECT username, ROUND( SUM( a.length_ms ) /1000 /60 ) AS minutes
+            FROM activity a
+            INNER JOIN activity_types t ON a.activity_type = t.id
+            WHERE a.day > DATE_SUB( CURDATE( ) , INTERVAL 1 WEEK )
+                AND a.activity_type NOT IN {}
+            GROUP BY a.username
+            ) AS m ON s.username = m.username
+        ORDER BY s.steps DESC
+        LIMIT 20""".format(bad_activities))
+    result = []
+    for r in db.fetchall():
+        result += [r['username'], int(r['steps']), int(r['minutes'])]
+    response.content_type = 'application/json'
+    return json.dumps(result, indent=4)
+
+
+@app.get('/set_goal/<name>/<goal>')
+def set_goal(name, goal, db):
+    require_key()
+    backend.get_logger().log_text("set_goal requested for: name={name}, goal={goal}".format(
+        name=name, goal=goal), severity='INFO')
+    db.execute("REPLACE INTO activity_goals SET username=%s, minutes=%s",
+               (name, goal))
+    return "Goal set"
+
+
+port = int(os.environ.get('PORT', 8080))
+prefix = os.environ.get('PREFIX', None)
+if prefix:
+    app.mount(prefix=prefix, app=app)
+
+if __name__ == "__main__":
+    try:
+        try:
+            app.run(host='0.0.0.0', port=port, debug=True, server='gunicorn',
+                    workers=2, timeout=1200)
+            print("app is now running using gunicorn server")
+        except ImportError:
+            app.run(host='0.0.0.0', port=port, debug=True)
+            print("app is now running NOT using gunicorn server")
+    except Exception as e:
+        print >> sys.stderr, "error: {}".format(e)
---
+++ googlecloud/fit/update_google_fit.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+import json
+from datetime import datetime
+
+import MySQLdb.cursors
+import httplib2
+import pytz
+from googleapiclient.discovery import build
+from oauth2client import client
+
+import backend
+
+
+def get_aggregate(fit_service, startTimeMillis, endTimeMillis, dataSourceId):
+    return fit_service.users().dataset().aggregate(userId="me", body={
+        "aggregateBy": [{
+            "dataTypeName": "com.google.step_count.delta",
+            "dataSourceId": dataSourceId
+        }],
+        "bucketByTime": {"durationMillis": backend.ONE_DAY_MS},
+        "startTimeMillis": startTimeMillis,
+        "endTimeMillis": endTimeMillis
+    }).execute()
+
+
+def get_and_store_fit_data(http_auth, cur, username, past_n_days=30):
+    fit_service = build('fitness', 'v1', http=http_auth)
+    n_days_ago_millis = backend.calc_n_days_ago(past_n_days)
+    #  print(help(fit_service.users().dataset().aggregate))
+    steps = []
+    activity = []
+    now_millis = backend.current_milli_time()
+    print('get and store fitness data for user {}'.format(username))
+    try:
+        stepsData = get_aggregate(fit_service, n_days_ago_millis, now_millis, backend.STEPS_DATASOURCE)
+        for day in stepsData['bucket']:
+            # store local date in the database
+            d = datetime.fromtimestamp(int(day['startTimeMillis']) / 1000,
+                                       tz=pytz.timezone(backend.DEFAULT_TIMEZONE)).strftime(backend.DATE_FORMAT)
+            if day['dataset'][0]['point']:
+                s = day['dataset'][0]['point'][0]['value'][0]['intVal']
+                steps.append([d, s])
+            else:
+                steps.append([d, 0])
+        print("Steps:", steps)
+    except Exception as e:
+        print(e)
+        print("No steps found")
+    try:
+        activityData = get_aggregate(fit_service, n_days_ago_millis, now_millis, backend.ACTIVITY_DATASOURCE)
+        for day in activityData['bucket']:
+            # store local date in the database
+            d = datetime.fromtimestamp(int(day['startTimeMillis']) / 1000,
+                                       tz=pytz.timezone(backend.DEFAULT_TIMEZONE)).strftime(backend.DATE_FORMAT)
+            if day['dataset'][0]['point']:
+                for a in day['dataset'][0]['point']:
+                    activity_type = a['value'][0]['intVal']
+                    length_ms = a['value'][1]['intVal']
+                    n_segments = a['value'][2]['intVal']
+                    activity.append([d, activity_type, length_ms, n_segments])
+            else:
+                activity.append([d, 4, 0, 0])
+        print("Activity:", activity)
+    except Exception as e:
+        print(e)
+        print("No activity found")
+    try:
+        rows = cur.executemany("REPLACE INTO steps SET username=%s, day=%s, steps=%s".format(username),
+                               [[username] + s for s in steps])
+        print("steps: {} rows affected".format(rows))
+        rows = cur.executemany(
+            "REPLACE INTO activity SET username=%s, day=%s, activity_type=%s, length_ms=%s, n_segments=%s",
+            [[username] + a for a in activity])
+        print("activity: {} rows affected".format(rows))
+    except Exception as e:
+        print(e)
+    return steps, activity
+
+
+if __name__ == "__main__":
+    with open(backend.get_client_secret_filename()) as f:
+        client_secret_json = json.load(f)
+        client_id = client_secret_json['web']['client_id']
+        client_secret = client_secret_json['web']['client_secret']
+
+    db = MySQLdb.connect(host=backend.config.get('database_config', 'dbhost'),
+                         port=int(backend.config.get('database_config', 'dbport')),
+                         user=backend.config.get('database_config', 'dbuser'),
+                         passwd=backend.config.get('database_config', 'dbpass'),
+                         db=backend.config.get('database_config', 'dbname'),
+                         cursorclass=MySQLdb.cursors.DictCursor)
+    cur = db.cursor()
+    n_rows = cur.execute("SELECT * FROM google_fit")
+    rows = cur.fetchall()
+    for r in rows:
+        username = r['username']
+        refresh_token = r['refresh_token']
+        creds = client.GoogleCredentials("", client_id, client_secret, refresh_token, 0,
+                                         "https://accounts.google.com/o/oauth2/token", "Python")
+        http_auth = creds.authorize(httplib2.Http())
+        try:
+            steps, activity = get_and_store_fit_data(http_auth, cur, username)
+        except Exception as e:
+            print("Unable to get fit data for {}! {}".format(username, e))
+        db.commit()
+    cur.close()
+    # disconnect from server
+    db.close()
