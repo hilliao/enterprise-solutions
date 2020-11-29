5b3e74436744692fbf914d215aca06aae340785b
Hil Liao <hilliao@gmail.com>
2020-11-29 09:09:58 -0800

    refactored auto delete python tool

--- python-utils/autodelete.py
+++ python-utils/autodelete.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-# Example crontab 0 3 * * * /home/hil/autodelete.py > /home/hil/autodelete.txt 2>&1 && echo "Finding and deleting empty directories..." >> /home/hil/autodelete.txt && find /mnt/1tb/ftp/hil -type d -empty -delete -print >> /home/hil/autodelete.txt 2>&1 && chown hil:hil /home/hil/autodelete.log && chown hil:hil /home/hil/autodelete.txt
+#
+# crontab does not support $HOME
+# replace $_HOME with /root, $WORKING_DIR with the directory to auto delete
+# Copy autodelete.py to $_HOME/bin
+# chmod a+x $_HOME/bin/autodelete.py
+#
+# Example crontab
+# 0 3 * * * $_HOME/bin/autodelete.py > $_HOME/bin/autodelete.txt 2>&1 && echo "Finding and deleting empty directories..." >> $_HOME/bin/autodelete.txt && find $WORKING_DIR -type d -empty -delete -print >> $_HOME/bin/autodelete.txt 2>&1
 import time, glob, os, logging
 
 # fresh start of log file
-LOG_FILENAME = '/home/hil/autodelete.log'
+LOG_FILENAME = '$_HOME/bin/autodelete.log'
 if os.path.isfile(LOG_FILENAME):
     os.remove(LOG_FILENAME)
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 # dir without trailing slash /
-parentdir = '/mnt/1tb/ftp/hil'
+parentdir = '$WORKING_DIR'
 
 for filename in glob.glob(os.path.join(parentdir, '**'), recursive=True):
     # skip the directory itself
@@ -19,6 +26,7 @@ for filename in glob.glob(os.path.join(parentdir, '**'), recursive=True):
     logging.debug(filename + ' is ' + str(timediff) + ' seconds old')
     # delete files and directories over 1 week
     week = 60 * 60 * 24 * 7
+
     if (timediff > week):
         try:
             if os.path.isfile(filename):
