88ec639e3788c39ad6116255348486a94bb4d970
Hil Liao <hilliao@gmail.com>
2021-06-18 18:17:47 -0700

    Enable cloud build trigger to apply a infrastructure deployment blueprint based on the substitutions user enters

---
+++ googlecloud/gke-config-sync/blueprints/build-triggers-gke.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # exit the script when execution hits any error
+set -x # print the executing lines
+
+
+NAME="${PWD##*/}-gke"
+REPO_NAME=
+BRANCH=master
+REPO_PATH="googlecloud/gke-config-sync/blueprints"
+
+gcloud beta builds triggers create cloud-source-repositories \
+    --repo=$REPO_NAME --name=$NAME \
+    --branch-pattern="^$BRANCH$" \
+    --build-config="$REPO_PATH/cloudbuild-gke.yaml" \
+    --included-files="$REPO_PATH/**" \
+    --ignored-files="**/README.md" \
+    --substitutions _GKE_CLUSTER_NAME=hil-blueprints-test,_DEPLOYMENT_NAME=hil-blueprints-test,_GKE_REGION=us-west1,_VPC=default,_SUBNET=default,_POD_IP_RANGE_NAME=gke-hil-pods,_SVC_IP_RANGE_NAME=gke-hil-services,_MASTER_IP_RANGE=172.22.32.0/28,_BLUEPRINTS_DIR=$REPO_PATH,_MACHINE_TYPE=e2-medium,_IF_PREEMPTIBLE=true,_DISK_GB=55
