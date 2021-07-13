204a1606cb69d61d5fe22df68db943d2ca1a5b1e
Hil Liao <hilliao@gmail.com>
2021-07-12 18:30:10 -0700

    migrated blueprints Kptfile to v1.0; fixed bugs in gcloud command to create private Google access DNS zones; fixed bugs in private catalog deployment which creates a cloud build trigger.

--- googlecloud/infrastructure/deployment-manager/private-catalog/private-catalog.py
+++ googlecloud/infrastructure/deployment-manager/private-catalog/private-catalog.py
@@ -1,7 +1,11 @@
 # Copyright 2021 GoogleCloud.fr; All rights reserved.
 #
-# Create a deployment manager action which execute a cloud build as a 1 time task
+# Create a deployment manager action which execute cloud build steps as a 1 time task
 # Expect the deployment to have a warning sign in the deployment manager page as action is not obsolete
+# Summary of the build steps:
+#   clone a public git repository
+#   create a new cloud source repository
+#   create a cloud build trigger referencing it
 
 def GenerateConfig(context):
     build_name = 'gcloud-exec'
@@ -23,25 +27,21 @@ def GenerateConfig(context):
                     'args': [
                         '-c',
                         f'echo is production? $$PRODUCTION && '
+                        f'apt-get update && apt-get -y install gettext-base && '
                         f'git clone https://github.com/hilliao/enterprise-solutions.git && '
                         f'gcloud source repos create {repo} --project {project_id} && '
                         f'cd enterprise-solutions && '
                         f'git config --global credential.https://source.developers.google.com.helper gcloud.sh && '
                         f'git remote add google https://source.developers.google.com/p/{project_id}/r/{repo} && '
                         f'git push --all google && '
-                        f'gcloud beta builds triggers create cloud-source-repositories \
-                            --repo={repo} --name={build_trigger} \
-                            --branch-pattern="^master$" \
-                            --build-config="$$REPO_PATH/cloudbuild-gke.yaml" \
-                            --included-files="$$REPO_PATH/**" \
-                            --ignored-files="**/README.md" \
-                            --substitutions _GKE_CLUSTER_NAME=hil-blueprints-test,_DEPLOYMENT_NAME=hil-blueprints-test,_GKE_REGION=us-west1,_VPC=default,_SUBNET=default,_POD_IP_RANGE_NAME=gke-hil-pods,_SVC_IP_RANGE_NAME=gke-hil-services,_MASTER_IP_RANGE=172.22.32.0/28,_BLUEPRINTS_DIR=$$REPO_PATH,_MACHINE_TYPE=e2-medium,_IF_PREEMPTIBLE=true,_DISK_GB=55 \
-                            --project {project_id}'
+                        f'export CSR_REPO={repo} && export CSR_REPO_PATH=$$REPO_PATH && export BUILD_PROJECT_ID={project_id} && export TRIGGER_NAME={build_trigger} && '
+                        f"envsubst '$$CSR_REPO,$$CSR_REPO_PATH,$$BUILD_PROJECT_ID,$$TRIGGER_NAME' < $$CSR_REPO_PATH/build-triggers-gke.sh > /workspace/build-triggers-gke.sh && "
+                        f"echo executing the following BASH script... && cat /workspace/build-triggers-gke.sh && sh /workspace/build-triggers-gke.sh"
 
                     ],
                     'env': [
                         'PRODUCTION=false',
-                        'REPO_PATH=googlecloud/gke-config-sync/blueprints'
+                        'REPO_PATH=googlecloud/infrastructure/blueprints'
                     ]
                 }
             ],
--- googlecloud/infrastructure/private-google-access.sh
+++ googlecloud/infrastructure/private-google-access.sh
@@ -7,17 +7,29 @@
 set -e # exit the script when execution hits any error
 set -x # print the executing lines
 
-declare -a domains=("googleapis.com" "googleadapis.com" "ltsapis.goog" "gcr.io" "pkg.dev" "gstatic.com" "appspot.com" "cloudfunctions.net" "pki.goog" "cloudproxy.app" "run.app" "datafusion.googleusercontent.com" "datafusion.cloud.google.com" "notebooks.cloud.google.com" "notebooks.googleusercontent.com" "appengine.google.com" "packages.cloud.google.com" "source.developers.google.com")
+declare -a domains=("googleadapis.com" "ltsapis.goog" "gcr.io" "pkg.dev" "gstatic.com" "appspot.com" "cloudfunctions.net" "pki.goog" "cloudproxy.app" "run.app" "datafusion.googleusercontent.com" "datafusion.cloud.google.com" "notebooks.cloud.google.com" "notebooks.googleusercontent.com" "appengine.google.com" "packages.cloud.google.com" "source.developers.google.com")
 
-PROJECT_ID=
-NETWORK=
+PROJECT_ID=agbg-blueprints-demo
+NETWORK=hil-test
+
+IPs="199.36.153.8 199.36.153.9 199.36.153.10 199.36.153.11"
+
+gcloud beta dns --project=$PROJECT_ID managed-zones create googleapis --description="https://cloud.google.com/vpc/docs/configure-private-google-access#config" --dns-name="googleapis.com." --visibility="private" --networks=$NETWORK && \
+gcloud beta dns --project=$PROJECT_ID record-sets transaction start --zone=googleapis
+gcloud beta dns --project=$PROJECT_ID record-sets transaction add private.googleapis.com. --name=\*.googleapis.com. --ttl=300 --type=CNAME --zone=googleapis
+gcloud beta dns --project=$PROJECT_ID record-sets transaction execute --zone=googleapis
+gcloud beta dns --project=$PROJECT_ID record-sets transaction start --zone=googleapis
+gcloud beta dns --project=$PROJECT_ID record-sets transaction add $IPs --name=private.googleapis.com. --ttl=300 --type=A --zone=googleapis
+gcloud beta dns --project=$PROJECT_ID record-sets transaction execute --zone=googleapis
 
 for domain in "${domains[@]}"
 do
+    # replace . with - for zone names
     NAME=$(echo $domain | tr . -)
+
     gcloud beta dns --project=$PROJECT_ID managed-zones create $NAME --description="https://cloud.google.com/vpc/docs/configure-private-google-access#config" --dns-name="$domain." --visibility="private" --networks=$NETWORK && \
     gcloud dns --project=$PROJECT_ID record-sets transaction start --zone=$NAME && \
-    gcloud dns --project=$PROJECT_ID record-sets transaction add 199.36.153.8 199.36.153.9 199.36.153.10 199.36.153.11 --name=$domain. --ttl=300 --type=A --zone=$NAME && \
+    gcloud dns --project=$PROJECT_ID record-sets transaction add $IPs --name=$domain. --ttl=300 --type=A --zone=$NAME && \
     gcloud dns --project=$PROJECT_ID record-sets transaction execute --zone=$NAME && \
     gcloud dns --project=$PROJECT_ID record-sets transaction start --zone=$NAME && \
     gcloud dns --project=$PROJECT_ID record-sets transaction add $domain. --name=\*.$domain. --ttl=300 --type=CNAME --zone=$NAME && \
