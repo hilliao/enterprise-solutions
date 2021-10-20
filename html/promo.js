987d6af1c7dd045dc1a45504fe2ef4c318187d7d
Hil Liao <hilliao@gmail.com>
2021-10-19 23:14:25 -0700

    Changed the redeem result logging to have explicit red,green colors with <div id=>

--- html/promo.js
+++ html/promo.js
@@ -23,12 +23,16 @@ $(document).ready(function () {
                 name: name,
             }),
             error: function (jqXHR, exception) {
-                $('.put_response').append(JSON.stringify(jqXHR));
-                $('.put_response').css("color", "red");
+                document.getElementById("put_response").innerHTML +=
+                    '<p style="color:red;">' + JSON.stringify(jqXHR) + '</p>'
             }
         }).then(function (put_res) {
+            var google_form = "Please proceed to submit <a href=" + put_res['form-url'] + ">the Google form</a> to complete the process"
+            document.getElementById("put_response").innerHTML +=
+                '<p style="color:green;">' + JSON.stringify(put_res) + '<br>' + google_form + '</p>'
+
             $('.put_response').append(JSON.stringify(put_res));
-            $('.put_response').append("Please proceed to submit <a href=" + put_res['form-url'] + ">the Google form</a> to complete the process");
+            $('.put_response').append();
             $('.put_response').css("color", "green");
         });
     });
