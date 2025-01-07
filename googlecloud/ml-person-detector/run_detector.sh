264db39e85732d9051d5bb485f017b173e3d8057
Hil <hilliao@gmail.com>
2025-01-06 23:40:12 -0800

    feat: upload detected images to Google cloud storage per settings in the environment variables.

---
+++ googlecloud/ml-person-detector/run_detector.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e # exit the script when execution hits any error
+set -x # print the executing lines
+
+export GOOGLE_APPLICATION_CREDENTIALS=confidential/secrets/person-detection-logger@gcp-project-id.iam.gserviceaccount.com
+export RTSP_URL="rtsp://user:password@192.168.111.42:554"
+export TFLITE_MODEL_PATH="$HOME/git/enterprise-solutions/googlecloud/ml-person-detector/efficientdet_lite2.tflite"
+export MAX_WORKERS=20
+export OUTPUT_IMAGE_DIR="/mnt/1tb/ftp/ipcam/autodelete/person-detector"
+export SKIP_X_FRAMES=3
+export OBJ_DETECT_CONFIDENCE_SCORE=0.4
+export PERSON_DETECT_CONFIDENCE_SCORE=0.5
+export GCS_BUCKET=bucket-name
+export GCS_FOLDER=us-amcrest-cam-0
